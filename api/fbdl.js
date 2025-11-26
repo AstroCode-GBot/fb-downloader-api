ffff4905cab21b00624bbd1526eb34b2bd3a09f2
AstroCode <astrocode.info@gmail.com>
2025-11-26 19:58:10 +0600

    Update fbdl.js

--- api/fbdl.js
+++ api/fbdl.js
@@ -1,1 +1,19 @@
+export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+
+  const url = req.query.url;
+  if (!url) {
+    return res.json({ status: "error", message: "No URL provided" });
+  }
+
+  try {
+    const api = "https://iamtarek.ct.ws/fbdw.php?url=" + encodeURIComponent(url);
+    const response = await fetch(api);
+    const json = await response.json();
+    return res.json(json);
+  } catch (err) {
+    return res.json({ status: "error", message: "Server error" });
+  }
+}
 
